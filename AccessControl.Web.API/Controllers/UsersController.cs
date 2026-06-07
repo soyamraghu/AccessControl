2dd37c91ee87cb148eeb3e967a5a25de63e6583d
soyamraghu <soyamraghu65@gmail.com>
2026-06-07 18:07:06 +0530

    Created_User_Apis

---
+++ AccessControl.Web.API/Controllers/UsersController.cs
@@ -0,0 +1,63 @@
+﻿using AccessControl.Web.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccessControl.Web.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UsersController(IUserService userService) 
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _userService.GetAllUsersAsync();
+            return Ok(users);
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            var user = await _userService.GetUserByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(Models.User user)
+        {
+            var createdUser = await _userService.CreateUserAsync(user);
+            return Ok (createdUser);
+        }
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, Models.User user)
+        {
+            var updatedUser = await _userService.UpdateUserAsync(id, user);
+            if (updatedUser == null)
+            {
+                return NotFound();
+            }
+            return Ok(updatedUser);
+        }
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var isDeleted = await _userService.DeleteUserAsync(id);
+            if (!isDeleted)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+    }
+}
