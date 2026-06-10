1a17095d6296a4e2a0f34bd6af73bc7f040ee6f5
bhagatprasad <bhagatprasad8@yahoo.com>
2026-06-10 11:40:31 +0530

    Jwt toekn read using filter and autherizaing from filter headers

--- AccessControl.Web.API/Models/Roles.cs
+++ AccessControl.Web.API/Models/Roles.cs
@@ -3,18 +3,18 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AccessControl.Web.API.Models
 {
-    [Table("Roles")]
+    [Table("Role")]
     public class Roles
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int RoleId { get; set; }
-        public string RoleName { get; set; }
-        public string Description { get; set; }
-        public bool IsActive { get; set; }
-        public string CreatedBy { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public string ModifiedBy { get; set; }
-        public DateTime ModifiedDate { get; set; }
+        public string RoleName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+        public string CreatedBy { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public string ModifiedBy { get; set; } = string.Empty;
+        public DateTime ModifiedDate { get; set; } = DateTime.UtcNow;
     }
 }
--- AccessControl.Web.DB/AccessControlDB/Tables/Role.sql
+++ AccessControl.Web.DB/AccessControlDB/Tables/Role.sql
@@ -1,11 +1,11 @@
-﻿CREATE TABLE [Roles]
+﻿CREATE TABLE [Role]
 (
-    RoleId INT IDENTITY(1,1) PRIMARY KEY,
-    RoleName VARCHAR(100) NOT NULL,
-    Description VARCHAR(255),
-    IsActive BIT NOT NULL DEFAULT 1,
-    CreatedBy VARCHAR(100),
-    CreatedDate DATETIME NOT NULL DEFAULT GETDATE(),
-    ModifiedBy VARCHAR(100),
-    ModifiedDate DATETIME
+    RoleId          INT                 IDENTITY(1,1)   PRIMARY KEY,
+    RoleName        VARCHAR(100)        NULL,
+    Description     VARCHAR(255)        NULL,
+    IsActive        BIT                 NULL DEFAULT 1,
+    CreatedBy       VARCHAR(100)        NULL,
+    CreatedDate     DATETIME            NULL DEFAULT GETDATE(),
+    ModifiedBy      VARCHAR(100)        NULL,
+    ModifiedDate    DATETIME            NULL
 );
